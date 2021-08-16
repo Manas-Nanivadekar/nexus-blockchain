62ef0dac2ccf752ba4cd7e23f6eafae2451824b7
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2021-08-16 23:45:40 +0530

    keys add

---
+++ key-insert/inset-key-one.sh
@@ -0,0 +1,2 @@
+curl https://one.rocksocket.xyz/ -H "Content-Type:application/json;charset=utf-8" -d "@node01-insert-aura.json"
+curl https://one.rocksocket.xyz/ -H "Content-Type:application/json;charset=utf-8" -d "@node01-insert-gran.json"
